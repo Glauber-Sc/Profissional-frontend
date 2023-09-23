44b8baa97a6a42e0823b75382bb79d76f0346c6c
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-23 17:57:42 -0300

    ATUALIZACAO DE IP httpS

--- src/services/apiPix.js
+++ src/services/apiPix.js
@@ -4,10 +4,10 @@ const apiPort4000 = axios.create({
   //  baseURL: "http://192.168.100.7:4000", // ou a URL correta do backend na porta 4000
 
 
- // baseURL: "https://177.71.203.249:4000",
+  baseURL: "https://177.71.203.249:4000",
 
 
-   baseURL: "https://pedepede.fun"
+   //baseURL: "https://pedepede.fun"
 
   });
   

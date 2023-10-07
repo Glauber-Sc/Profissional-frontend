bc3540b7deb6d7a34ac9b15c13cf6cdcf26635b8
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-07 15:17:14 -0300

    URL 9

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,26 @@
 /* Arquivo de serviços para acessar o axios */
-import axios from 'axios'
+import axios from "axios";
 
 const apiCodeBurger = axios.create({
 
-   baseURL: "http://177.71.203.249:3000",
+  baseURL: "https://177.71.203.249:3000",
 
   // baseURL: 'http://192.168.100.7:3000',
 
   //baseURL: 'https://api-hambuguer.coders4future.com'
 
   //baseURL: 'https://pedepede.fun'
-})
+
+});
 
 // Interceptando um arequisição. Antes de enviar a requisição vai ser Adicionado Token JWT nas chamadas da API
-apiCodeBurger.interceptors.request.use(async config => {
+apiCodeBurger.interceptors.request.use(async (config) => {
   // Pegando o token no localStorage
-  const userData = await localStorage.getItem('codeburger: userData')
-  const token = userData && JSON.parse(userData).token
+  const userData = await localStorage.getItem("codeburger: userData");
+  const token = userData && JSON.parse(userData).token;
   // Colocando o token dentro da config
-  config.headers.authorization = `Bearer ${token}`
-  return config
-})
+  config.headers.authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default apiCodeBurger
+export default apiCodeBurger;

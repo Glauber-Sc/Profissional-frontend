99f7cb573379664bc8763e6c7f18203505c9d3d6
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-10 21:02:51 -0300

    zip oficial

--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -66,7 +66,7 @@ export const Input = styled.input`
 export const ButtonLogin = styled.button`
   width: 11.426rem;
   height: 3rem;
-  background:  #ff0000;
+  background:  #000;
   border-radius: 1.25rem;
   font-style: normal;
   font-weight: 500;
